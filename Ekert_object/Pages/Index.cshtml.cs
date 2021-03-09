d6803f0d6167f05b8a319c7ca868d1eb89bad641
Tomáš Ekert <u19.408@pslib.cloud>
2021-03-09 08:18:08 +0100

    Přidejte soubory projektu.

---
+++ Ekert_object/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ekert_object.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public int Povrch { get; set; }
+        public int Objem { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnPost(int A, int B, int C)
+        {
+
+            if (A >= 0 & B >= 0 & C >= 0)
+            {
+                Objem = A * B * C;
+                Povrch = 2 * (A * B + B * C + A * C);
+            }
+            else
+            {
+            }
+        }
+    }
+}
